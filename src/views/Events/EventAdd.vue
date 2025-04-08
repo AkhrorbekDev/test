6bfbbaf66be7b26ec644a08adb24dae9767768d6
axror <axrorbeknis@icloud.com>
2025-04-09 00:14:41 +0500

    asd

--- src/views/Events/EventAdd.vue
+++ src/views/Events/EventAdd.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import { createUserService } from '@/services'
 import { useToast } from 'vue-toastification'
+import { VDateInput } from 'vuetify/labs/VDateInput'
 
 const toast = useToast()
 
@@ -12,8 +13,13 @@ const eventForm = ref({
   type: 'game',
   date: '',
   org_name: '',
-  org_bio: ''
+  org_bio: '',
+  startTime: '',
+  maxPlayers: '',
+  endTime: ''
 })
+const menu2 = ref(false)
+const menu3 = ref(false)
 const handleFileUpload = (event) => {
   const file = event.target.files[0]
   if (file) {
@@ -38,10 +44,14 @@ const handleFileUpload = (event) => {
         Создать событие
       </h1>
       <div class="event-types">
-        <button class="event-types__type">
+        <button class="event-types__type" :class="{
+          _active: eventForm.type === 'game'
+        }" @click="eventForm.type = 'game'">
           Игровая сессия
         </button>
-        <button class="event-types__type">
+        <button class="event-types__type" :class="{
+        _active: eventForm.type === 'event'
+        }" @click="eventForm.type = 'event'">
           Мероприятие
         </button>
       </div>
@@ -61,11 +71,12 @@ const handleFileUpload = (event) => {
             </div>
           </div>
           <div class="input__wrapper">
-            <label for="desc">Краткое описание*</label>
-            <div class="input__group">
-              <input v-model="eventForm.description" type="text" name="desc" id="desc"
-                     placeholder="Напишите здесь что-нибудь">
-            </div>
+            <label for="about_you">Расскажите о себе</label>
+
+            <textarea v-model="eventForm.description" type="text" name="about_you" id="about_you"
+                      placeholder="Пусть весь мир узнает"></textarea>
+            <div class="input__wrapper-txt">Не более 350 символов, включая пробелы</div>
+
           </div>
         </div>
         <div class="event-main__image">
@@ -82,6 +93,155 @@ const handleFileUpload = (event) => {
       </div>
     </div>
 
+    <div class="create-event__form">
+      <h3>
+        Подробности меропрития
+      </h3>
+      <div class="event-main__desc">
+        <div class="input__wrapper">
+          <label for="about_you">Расскажите о себе</label>
+
+          <textarea v-model="eventForm.description" type="text" name="about_you" id="about_you"
+                    placeholder="Пусть весь мир узнает"></textarea>
+          <div class="input__wrapper-txt">Не более 350 символов, включая пробелы</div>
+
+        </div>
+        <div class="input__wrapper">
+          <label for="about_you">Расскажите о себе</label>
+
+          <textarea v-model="eventForm.description" type="text" name="about_you" id="about_you"
+                    placeholder="Пусть весь мир узнает"></textarea>
+          <div class="input__wrapper-txt">Не более 350 символов, включая пробелы</div>
+
+        </div>
+      </div>
+    </div>
+    <div class="create-event__form">
+      <h3>
+        Дата и время проведения события
+      </h3>
+
+      <div class="event-main__dates">
+        <div class="input__wrapper">
+          <label for="date">Дата*</label>
+          <div class="input__group">
+            <v-date-input label="Date input" variant="solo-filled"></v-date-input>
+          </div>
+        </div>
+        <div class="input__wrapper">
+          <label for="time">Время начала*</label>
+          <div class="input__group">
+            <v-text-field
+              v-model="eventForm.startTime"
+              :active="menu2"
+              :focus="menu2"
+              label="Picker in menu"
+              prepend-icon="mdi-clock-time-four-outline"
+              readonly
+            >
+              <v-menu
+                v-model="menu2"
+                :close-on-content-click="false"
+                activator="parent"
+                transition="scale-transition"
+              >
+                <v-time-picker
+                  v-if="menu2"
+                  v-model="eventForm.startTime"
+                  full-width
+                ></v-time-picker>
+              </v-menu>
+            </v-text-field>
+          </div>
+        </div>
+        <div class="input__wrapper">
+          <label for="time">Окончание события*</label>
+          <div class="input__group">
+            <v-text-field
+              v-model="eventForm.endTime"
+              :active="menu3"
+              :focus="menu3"
+              label="Picker in menu"
+              prepend-icon="mdi-clock-time-four-outline"
+              readonly
+            >
+              <v-menu
+                v-model="menu3"
+                :close-on-content-click="false"
+                activator="parent"
+                transition="scale-transition"
+              >
+                <v-time-picker
+                  v-if="menu3"
+                  v-model="eventForm.endTime"
+                  full-width
+                ></v-time-picker>
+              </v-menu>
+            </v-text-field>
+          </div>
+        </div>
+        <div class="input__wrapper">
+          <label for="">Место</label>
+          <div class="input__group">
+            <input v-model="eventForm.place" type="text" placeholder="Место">
+            <i class="fal fa-chevron-down"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="create-event__form">
+      <h3>
+        Организационные моменты
+      </h3>
+      <div class="event-main__orginfo">
+        <div class="input__wrapper">
+          <label for="title">Стоимость участия*</label>
+          <div class="input__group">
+            <input v-model="eventForm.price" type="text" name="title" id="title"
+                   placeholder="от 0 до 100 000 ₽ ">
+          </div>
+        </div>
+        <div class="input__wrapper">
+          <label for="title">Размер скидки, %</label>
+          <div class="input__group">
+            <input v-model="eventForm.discount" type="text" name="title" id="title"
+                   placeholder="от 0 до 100">
+          </div>
+        </div>
+        <div class="input__wrapper">
+          <label for="title">Макс. количество игроков*</label>
+          <div class="input__group">
+            <input v-model="eventForm.maxPlayers" type="text" name="title" id="title"
+                   placeholder="от 1 до 100">
+          </div>
+        </div>
+        <div class="input__wrapper">
+          <label for="title">Запись на событие*</label>
+          <div class="input__group">
+            <input v-model="eventForm.regType" type="text" name="title" id="title"
+                   placeholder="Открытая или закрытая">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="create-event__form">
+      <div class="create-event__totals">
+        <div class="event-total__price">
+          <p class="price-title">Итоговая цена с участника</p>
+          <p class="price">
+            {{ eventForm.price }}
+          </p>
+        </div>
+        <div class="event-total__price">
+          <p class="price-title">Формат события</p>
+          <p class="price">
+            Запись {{ eventForm.regType }}, {{ eventForm.maxPlayers }} игроков
+          </p>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -90,99 +250,182 @@ const handleFileUpload = (event) => {
 .create-event {
   padding: 145px 10px !important;
 
+  h3 {
+    font-weight: 500;
+    font-size: 32px;
+    line-height: 114.99999999999999%;
+    letter-spacing: 0;
+    color: #815692;
+    margin-bottom: 24px;
+
+  }
+
   &__header {
     display: flex;
     align-items: center;
     justify-content: space-between;
+    margin-bottom: 30px;
+
     .title {
       color: #eee0f1 !important;
       width: 100%;
       font-size: calc(32px + 0.7291666667vw);
     }
+
     .event-types {
+      //width: 380px;
+      height: 55px;
+      border-radius: 10px;
       display: flex;
+      align-items: center;
+      overflow: hidden;
+
+      &__type {
+        width: 190px;
+        height: 55px;
+        justify-content: space-between;
+        padding-top: 12px;
+        padding-right: 16px;
+        white-space: nowrap;
+        padding-bottom: 12px;
+        padding-left: 16px;
+        background: #18171E;
+        color: #D5B0E4;
+        border: 1px solid #1F1821;
+
+        &._active {
+          background: #422748;
+          color: #eee0f1;
+        }
+      }
     }
   }
 
   &__form {
+    margin-bottom: 50px;
 
-    .event-main__info {
-      display: flex;
-      align-items: flex-end;
-      gap: 16px;
-      margin-top: 24px;
-
-      .event-main__titles {
+    .event-main {
+      &__desc, &__dates, &__orginfo {
         display: flex;
-        flex-direction: column;
-        gap: 31px;
-        width: 100%;
-
-        .input__wrapper {
-          width: 100%;
-        }
+        align-items: center;
+        gap: 20px;
       }
 
-      .event-main__image {
+      &__info {
         display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 100%;
-        height: 200px;
-        border-radius: 16px;
+        align-items: flex-end;
+        gap: 16px;
+        margin-top: 24px;
 
-        .settings__top-left {
-          width: 100%;
-          background: var(--03-dark-2, #18171e);
-          border-radius: 5px;
-          border-style: dashed;
-          border-color: #321939;
-          border-width: 1px;
-
+        .event-main__titles {
           display: flex;
           flex-direction: column;
-          align-items: center;
-          justify-content: center;
-          padding: 32px 0;
+          gap: 31px;
+          width: 100%;
 
-
-          @media (max-width: 990px) {
-            max-width: 100%;
-          }
-
-          img {
-            margin-bottom: 12px;
-          }
-
-          .settings__left-title {
-            color: var(--04-text-main, #eee0f1);
-            text-align: center;
-            font-size: var(--pc-button-1-font-size, 20px);
-            margin-bottom: 33px;
-          }
-
-          .settings__left-subtitle {
-            color: #815692;
-            text-align: center;
-            font-size: var(--pc-helper-text-font-size, 14px);
-            max-width: 204px;
+          .input__wrapper {
             width: 100%;
           }
         }
 
-        label {
-          cursor: pointer;
+        .event-main__image {
           display: flex;
-          flex-direction: column;
-          align-items: center;
           justify-content: center;
+          align-items: center;
           width: 100%;
           height: 100%;
-          text-align: center;
+          border-radius: 16px;
 
-          img {
-            margin-bottom: 8px;
+          .settings__top-left {
+            width: 100%;
+            background: var(--03-dark-2, #18171e);
+            border-radius: 5px;
+            border-style: dashed;
+            border-color: #321939;
+            border-width: 1px;
+
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            padding: 32px 0;
+
+
+            @media (max-width: 990px) {
+              max-width: 100%;
+            }
+
+            img {
+              margin-bottom: 12px;
+            }
+
+            .settings__left-title {
+              color: var(--04-text-main, #eee0f1);
+              text-align: center;
+              font-size: var(--pc-button-1-font-size, 20px);
+              margin-bottom: 33px;
+            }
+
+            .settings__left-subtitle {
+              color: #815692;
+              text-align: center;
+              font-size: var(--pc-helper-text-font-size, 14px);
+              max-width: 204px;
+              width: 100%;
+            }
           }
+
+          label {
+            cursor: pointer;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            width: 100%;
+            height: 100%;
+            text-align: center;
+
+            img {
+              margin-bottom: 8px;
+            }
+          }
+        }
+      }
+
+
+    }
+  }
+
+  &__totals {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+
+    .event-total {
+      &__price {
+        width: 100%;
+        height: 126px;
+        gap: 6px;
+        border-radius: 5px;
+        padding: 20px;
+        background: linear-gradient(114.17deg, rgba(54, 38, 38, 0.3) -3.5%, rgba(34, 27, 36, 0.3) 47.08%, rgba(54, 38, 38, 0.3) 97.65%);
+        border: 1px solid #1F1821;
+
+        .price-title {
+          font-weight: 400;
+          font-size: 26px;
+          line-height: 114.99999999999999%;
+          letter-spacing: 0%;
+          color: linear-gradient(90deg, #CBBECD 0%, #FFFFFF 46.5%, #CBBECD 100%);
+
+        }
+
+        .price {
+          font-weight: 500;
+          font-size: 46px;
+          line-height: 100%;
+          letter-spacing: 0%;
+
         }
       }
     }
@@ -203,6 +446,7 @@ const handleFileUpload = (event) => {
   label {
     color: #eee0f1;
     font-size: 22px;
+    white-space: nowrap;
 
     span {
       color: #f0515e;
--- src/views/MainPage/components/StepsComponent.vue
+++ src/views/MainPage/components/StepsComponent.vue
@@ -15,7 +15,7 @@
                 <div class="steps__pirates-item">
                     <div class="steps__pirates-top">
                         <h3 class="steps__pirates-title">Запишитесь на игру</h3>
-                        <img src="../images/steps/edit.svg" alt="">
+<!--                        <img src="/images/steps/edit.svg" alt="">-->
                     </div>
                     <div class="steps__pirates-txt"> В нашем расписании выберите игру для новичков или обратитесь
                         к <span>Привратнику</span> за советом —
@@ -26,7 +26,7 @@
                 <div class="steps__pirates-item">
                     <div class="steps__pirates-top">
                         <h3 class="steps__pirates-title">Поговорите с Мастером</h3>
-                        <img src="../images/steps/people.svg" alt="">
+<!--                        <img src="/images/steps/people.svg" alt="">-->
                     </div>
                     <div class="steps__pirates-txt">Перед игрой вы сможете познакомиться с Мастером, который объяснит
                         правила, поможет создать персонажа и ответит на все вопросы</div>
@@ -35,7 +35,7 @@
                 <div class="steps__pirates-item">
                     <div class="steps__pirates-top">
                         <h3 class="steps__pirates-title">Создайте персонажа</h3>
-                        <img src="../images/steps/robot.svg" alt="">
+<!--                        <img src="/images/steps/robot.svg" alt="">-->
                     </div>
                     <div class="steps__pirates-txt">Это ваш герой, которым вы будете управлять во время игры. Его можно
                         сделать вместе с Мастером перед игрой или просто выбрать готового из предложенных,
@@ -45,7 +45,7 @@
                 <div class="steps__pirates-item">
                     <div class="steps__pirates-top">
                         <h3 class="steps__pirates-title">Вникайте по ходу игры</h3>
-                        <img src="../images/steps/three.svg" alt="">
+<!--                        <img src="/images/steps/three.svg" alt="">-->
                     </div>
                     <div class="steps__pirates-txt">
                         Вам не нужно знать все правила заранее. Мастер будет вести игру и подсказывать вам, что делать
@@ -56,7 +56,7 @@
                 <div class="steps__pirates-item">
                     <div class="steps__pirates-top">
                         <h3 class="steps__pirates-title">Наслаждайтесь процессом!</h3>
-                        <img src="../images/steps/edit.svg" alt="">
+<!--                        <img src="/images/steps/edit.svg" alt="">-->
                     </div>
                     <div class="steps__pirates-txt">Самое важное — это просто расслабиться и получать удовольствие.
                         Не бойтесь задавать вопросы и экспериментировать!</div>
@@ -68,7 +68,7 @@
             <div class="steps__bottom">
                 <div class="steps__bottom-txt">Наш клуб всегда рад новичкам и готов помочь вам сделать первые шаги
                     в удивительный мир настольных ролевых игр!</div>
-                <img src="../images/steps/sword.svg" alt="">
+<!--                <img src="/images/steps/sword.svg" alt="">-->
             </div>
 
         </div>
